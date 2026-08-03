d5fd5bd60a4bc222855445cee8a65cb8cd2d25ae
Naman Jain <namanjainb3@gmail.com>
2026-08-03 16:15:28 +0530

    Update authMiddleware.js

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,32 +1,28 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
+  const token = req.headers.authorization?.split(" ")[1];
 
-    const token = req.headers.authorization?.split(" ")[1];
+  console.log("TOKEN:", token);
+  console.log("JWT_SECRET:", process.env.JWT_SECRET);
 
-    if (!token) {
-        return res.status(401).json({
-            message: "No token"
-        });
-    }
+  if (!token) {
+    return res.status(401).json({ message: "No token" });
+  }
 
-    try {
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-        const decoded = jwt.verify(
-            token,
-            process.env.JWT_SECRET
-        );
+    console.log("Decoded:", decoded);
 
-        req.user = decoded;
+    req.user = decoded;
+    next();
+  } catch (err) {
+    console.log("JWT ERROR:", err.name);
+    console.log("JWT MESSAGE:", err.message);
 
-        next();
-
-    } catch {
-
-        return res.status(401).json({
-            message: "Invalid token"
-        });
-
-    }
-
+    return res.status(401).json({
+      message: err.message,
+    });
+  }
 };
